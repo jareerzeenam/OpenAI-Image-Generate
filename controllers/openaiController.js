ee621e43a55aacfa23531ccbe67e675edc8061cc
jareer <jareerzeenam@adventus.io>
2022-12-14 13:47:20 +0530

    Backend image processing feature implemented

---
+++ controllers/openaiController.js
@@ -0,0 +1,37 @@
+const { Configuration, OpenAIApi } = require('openai');
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const generateImage = async (req, res) => {
+  try {
+    const response = await openai.createImage({
+      prompt: 'Parrot on a tree', // Input test to process image
+      n: 1, // Number of images
+      size: '512x512', // Image size
+    });
+
+    const imageUrl = response.data.data[0].url;
+
+    res.status(200).json({
+      success: true,
+      data: imageUrl,
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+
+    res.status(400).json({
+      success: false,
+      error: 'The image could not be generated',
+    });
+  }
+};
+
+module.exports = { generateImage };
